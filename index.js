f360ddcc13fd6a025e5f8f46687b1962df6c3955
Sam Hill <sam.hudhill@gmail.com>
2021-09-15 12:28:35 +0200

    route for creating a new short url

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
-// Connect to database
+// Connect to database:
 connectDB();
 
 app.use(express.json({extended: false}));
+
+//Routes:
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
 
 app.listen(5000, function () {
     console.log(`Server running on port ${this.address().port}`);
